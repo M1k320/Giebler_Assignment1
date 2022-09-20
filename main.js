3953cd5d55dec8afa682d3e5dfffa979bfd2f29c
Michael Giebler <megiebler@mail.fhsu.edu>
2022-09-19 20:30:21 -0500

    second commit

---
+++ main.js
@@ -0,0 +1,25 @@
+//Michael Giebler Assignment 1
+
+//Question 1
+var myNumber;
+//Question 2
+var myNumber = 5;
+//Question 3
+var myOtherNumber;
+myOtherNumber = 5
+//Question 4
+var myTotal;
+myTotal = myNumber * myOtherNumber;
+//Question 5
+var myName = ""; 
+var myCombo = myNumber + myOtherNumber
+//Question 6
+let myOtherCombo = myNumber + myOtherNumber;
+//Question 7
+let myRemainder = myNumber % 5;
+//Question 8
+let i = 2;
+//Question 9 
+let int = 4;
+//question 10 
+let myBoolean = false;
